82509e93b955885cd3fd7932d7cd6178fcf242d3
Tomasz Kłos <xilent98@wp.pl>
2022-10-16 20:18:20 +0200

    add prepared data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = [
+    {
+        id: 'a1',
+        title: 'Task 1',
+        status: 'completed',
+        businessContexts: [
+            {},
+        ],
+    },
+    {
+        id: 'a2',
+        title: 'Task 2',
+        status: 'completed',
+        businessContexts: [
+            {},
+        ],
+    },
+    {
+        id: 'a3',
+        title: 'Task 3',
+        status: 'completed',
+        businessContexts: [
+            {},
+        ],
+    },  
+    {
+        id: 'a3',
+        title: 'Task 4',
+        status: 'active',
+        businessContexts: [
+            {},
+        ],
+    },  
+    {
+        id: 'a5',
+        title: 'Task 5',
+        status: 'blocked',
+        businessContexts: [
+            {},
+        ],
+    },  
+];
